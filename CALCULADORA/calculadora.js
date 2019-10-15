743330bc5fe6a2845a29a6c54c766125e4a597ad
Jorge Rodriguez <jorguero47@gmail.com>
2019-10-15 20:44:49 +0200

    Calculadora acabada con bugs,pero acabada xD

--- CALCULADORA/calculadora.js
+++ CALCULADORA/calculadora.js
@@ -4,9 +4,11 @@ var haySigno;
 var numCalculado; //Será el resultado que se actualizará cuando hayan 2 nums;
 var signo;
 var operacion;
+var pantalla = document.getElementById("pantalla");
 
 var num1;
 var num2;
+
 
 
 window.onload = function() {
@@ -37,132 +39,151 @@ function insertarNumero(boton) {
 
     var numero = this.innerText;
     esNum = true;
-   
-    
 
- 
+
+
+
+
     if (!hayComa && numero == ".") {
         document.getElementById("pantalla").value += ".";
         hayComa = true;
-    } 
+    }
 
-    if ( numero == "+" && hayNum) {
-        num1 = document.getElementById("pantalla").value;
+    if (numero == "+" && hayNum) {
         document.getElementById("pantalla").value += "+";
         signo = "+";
         hayNum = false;
-      
+
     }
 
-    if ( numero == "-" && hayNum) {
-        num1 = document.getElementById("pantalla").value;
+    if (numero == "-" && hayNum) {
         document.getElementById("pantalla").value += "-";
         signo = "-";
         hayNum = false;
 
-    } if ( numero == "x" && hayNum) {
-           num1 = document.getElementById("pantalla").value;
+    }
+    if (numero == "x" && hayNum) {
         document.getElementById("pantalla").value += "x";
         signo = "x";
         hayNum = false;
     }
 
-    if ( numero == "/" && hayNum) {
-        num1 = document.getElementById("pantalla").value;
+    if (numero == "/" && hayNum) {
         document.getElementById("pantalla").value += "/";
         signo = "/";
         hayNum = false;
     }
 
-    if ( numero == "%" && hayNum) {
-        num1 = document.getElementById("pantalla").value;
+    if (numero == "%" && hayNum) {
+
         document.getElementById("pantalla").value += "%";
         signo = "%";
         hayNum = false;
     }
 
- if (numero != "." && numero != "+" && numero != "-" && numero != "/" && numero != "x" && numero != "%") {
-    
-    num2 = document.getElementById("pantalla").value.split([signo],[document.getElementById("pantalla").value.length]);
-   
-    if (document.getElementById("pantalla").value == "0") {
-
-        document.getElementById("pantalla").value = numero;
-        
-    } else {
-        document.getElementById("pantalla").value += numero;
-    } 
-      
-    hayNum = true;
-    
-   }
-
-   if (numero == "=") {
-       calcular(num1,signo, num2[1]);
-   }
-
- 
-    calcularSegundoPlano(num1,signo, num2[1]);
-   
- }
-
-
-
-
-function calcular(num1,signo, num2){
-    
-    switch (signo) {
-        case "+":
-            numCalculado = parseInt(num1) + parseInt(num2);
-        break;
-    
-        case "-":
-            numCalculado = num1 - num2;
-        break;
-
-        case "x":
-            numCalculado = num1 * num2;
-        break;
-
-        case "/":
-            numCalculado = num1 / num2;
-        break;
-
-        case "%":
-            numCalculado = num1 % num2;
-        break;
+    /*if (numero == "C") {
+        resetear();
     }
 
-    //document.getElementById("pantalla").value =numCalculado;
-    console.log(numCalculado);
-    num1 = numCalculado;
-    numCalculado = "";
+    if (numero == "«") {
+        retroceder();
+    }
+
+    if (numero == "«") {
+        retroceder();
+    }*/
+
+
+
+
+    if (numero != "." && numero != "+" && numero != "-" && numero != "/" && numero != "x" && numero != "%" && numero != "C" && numero != "«") {
+
+        operacion = document.getElementById("pantalla").value;
+
+
+        if (document.getElementById("pantalla").value == "0") {
+
+            document.getElementById("pantalla").value = numero;
+
+        } else {
+            document.getElementById("pantalla").value += numero;
+        }
+
+        hayNum = true;
+
+    }
+
+
+
+    switch (numero) {
+
+        case "C":
+            resetear();
+            break;
+
+        case "«":
+            retroceder();
+            break;
+
+        case "()":
+            parentesis(operacion);
+            break;
+
+    }
+
+
+    if (signo == "%" && numero == "=") {
+        porcentaje(operacion, signo);
+    } else if (numero == "=") {
+        calcular(operacion, "%");
+    }
+
+
 }
 
 
-function calcularSegundoPlano(num1,signo, num2){
-    switch (signo) {
-        case "+":
-            numCalculado = parseInt(num1) + parseInt(num2);
-        break;
-    
-        case "-":
-            numCalculado = num1 - num2;
-        break;
+//document.getElementById("pantalla")
 
-        case "x":
-            numCalculado = num1 * num2;
-        break;
+function calcular(numero, signo) {
 
-        case "/":
-            numCalculado = num1 / num2;
-        break;
 
-        case "%":
-            numCalculado = num1 % num2;
-        break;
+    numero = numero.replace("x", "*");
+
+    numCalculado = eval(numero);
+    document.getElementById("pantalla").value = numCalculado;
+    num1 = numCalculado;
+
+}
+
+function resetear() {
+
+    operacion = "";
+    numCalculado = "";
+    document.getElementById("pantalla").value = "0";
+
+
+}
+
+function retroceder() {
+
+    document.getElementById("pantalla").value = document.getElementById("pantalla").value.slice(document.getElementById("pantalla").length, -1);
+    if (document.getElementById("pantalla").value.length <= 0) {
+        document.getElementById("pantalla").value = "0";
     }
+}
 
-   
-    console.log(numCalculado);
+function porcentaje(operacion, signo) {
+
+
+    var num1 = operacion.split(["%"], 1);
+    var aux = operacion.split(["%"], operacion.length);
+    var num2 = aux[1];
+
+    numCalculado = ((num1 * num2) / 100);
+    document.getElementById("pantalla").value = numCalculado;
+}
+
+function parentesis(operacion) {
+    operacion = "(" + operacion + ")";
+    document.getElementById("pantalla").value = operacion;
 }
