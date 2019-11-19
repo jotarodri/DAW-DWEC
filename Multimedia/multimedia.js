cbc318301b05af641f79cc37fe2103d9c64c0170
Jorge Rodriguez <jorguero47@gmail.com>
2019-11-19 20:31:25 +0100

    Inicio del reproductor

--- Multimedia/multimedia.js
+++ Multimedia/multimedia.js
@@ -1,9 +1,24 @@
 window.onload = function() {
     
 document.querySelector("video").addEventListener("click",ejecutarVideo);
+crearDivs();
 
 }
 
 function ejecutarVideo() {
     this.play();
+}
+
+function crearDivs() {
+    
+var containerVideos = document.getElementById("containerVideos");
+
+for (let i = 0; i < 6; i++) {
+    
+    let div = document.createElement("div");
+    div.classList.add("miniVideo");
+    containerVideos.appendChild(div);
+
+}
+
 }
