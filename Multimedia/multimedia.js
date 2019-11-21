add87ec91aa01398e4d4ae1716106982581e43ef
Jorge Rodriguez <jorguero47@gmail.com>
2019-11-21 20:39:38 +0100

    Faltan añadir mas videos

--- Multimedia/multimedia.js
+++ Multimedia/multimedia.js
@@ -1,24 +1,80 @@
+var pausado = false;
+var silenciado = false;
+
 window.onload = function() {
-    
-document.querySelector("video").addEventListener("click",ejecutarVideo);
-crearDivs();
+        
+
+document.querySelector(".play").addEventListener("click",ejecutarVideo);
+document.querySelector(".silenciar").addEventListener("click",silenciarVideo);
+document.querySelector(".retroceder").addEventListener("click",retrocederVideo);
+document.querySelector(".avanzar").addEventListener("click",avanzarVideo);
+document.querySelector(".reiniciar").addEventListener("click",reiniciarVideo);
+document.querySelector(".bajarvol").addEventListener("click",bajarVolumen);
+document.querySelector(".subirvol").addEventListener("click",subirVolumen);
 
 }
 
 function ejecutarVideo() {
-    this.play();
-}
-
-function crearDivs() {
     
-var containerVideos = document.getElementById("containerVideos");
-
-for (let i = 0; i < 6; i++) {
+    let video = document.querySelector("video");
+    if (!pausado) {
+        video.play();    
+        pausado = true;
+    }else{
+        video.pause();
+        pausado = false;
+    }
     
-    let div = document.createElement("div");
-    div.classList.add("miniVideo");
-    containerVideos.appendChild(div);
+}
+
+function silenciarVideo() {
+    
+    let video = document.querySelector("video");
+    
+    if (!silenciado) {
+        video.muted= true;
+        silenciado = true;
+    }else{
+        video.muted = false;
+        silenciado = false;
+    }
 
 }
 
+function retrocederVideo() {
+    
+    let video = document.querySelector("video");
+
+    video.currentTime -= 10;
 }
+
+function avanzarVideo() {
+    
+    let video = document.querySelector("video");
+
+    video.currentTime += 10;
+}
+
+function reiniciarVideo() {
+    
+    let video = document.querySelector("video");
+
+    video.currentTime = 0;
+    video.play();
+}
+
+function bajarVolumen() {
+    
+    let video = document.querySelector("video");
+
+    video.volume -= 0.1;
+}
+
+function subirVolumen() {
+    
+    let video = document.querySelector("video");
+
+    video.volume +=0.1;
+}
+
+
